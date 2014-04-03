a78b04f786579d6f1ded3c521d4b0798d41df158
Alin Draghia <alin.draghia@google.com>
2014-04-03 15:03:31 +0300

    added new reservation page form + style

--- newreservation.php
+++ newreservation.php
@@ -3,8 +3,55 @@
 
 <?php
 function main() {
-    print("hello world!");   
+    return null;
 }
 ?>
+
+<?php main(); ?>
+
+
+<div id="reservation-form">
+    <form method="post" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <fieldset>
+            <legend>Reservation Form</legend>
+            
+            <p>
+            <label>Hotel:</label>
+            <input type="text" readonly="true" value="[Ambasador]"/>
+            </p>
+            
+            <p>
+            <label>Room type:</label>
+            <input type="text" readonly="true" value="[Single]"/>
+            </p>
+            
+            <p>
+            <label>Price per night:</label>
+            <input type="text" readonly="true" value="[100$]"/>
+            </p>
+            
+            <p>
+            <label for="start_date">Start date:</label>
+            <input type="date" name="start_date" value=""/>
+            </p>
+            
+            <p>
+            <label for="end_date">End date:</label>
+            <input type="date" name="end_date" value=""/>
+            </p>
+            
+            <p>
+            <label for="number_of_rooms">Number of rooms:</label>
+            <input type="number" name="number_of_rooms" value="1" min="1" step="1"/>
+            </p>
+            
+            <span>
+            <input type="submit" value="Submit"/>
+            <input type="reset" value="Reset"/>
+            </span>
+        </fieldset>
+    </form>
+</div>
+
 
 <?php include 'footer.php'; ?>
